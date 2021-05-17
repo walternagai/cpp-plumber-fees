33c7ff91bd320cb1f4498e64d0a92a49b07f0554
Walter Aoiama Nagai <walternagai@unifei.edu.br>
2021-05-17 15:50:58 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,6 @@
 using namespace std;
 
 int main() {
-  // crie duas variáveis dolarEuro, realDolar
-  
-  // crie duas variáveis investimentoEuros, investimentoDolares, investimentoReais
 
-  // leia a variável investimentoEuros
-  
-  // leia a variável dolarEuro
-
-  // leia a variável realDolar
-  
-  // calcule investimentoDolares
-
-  // calcule investimentoReais
-
-  // mostre o valor investimentoReais
-  
   return 0;
 }
